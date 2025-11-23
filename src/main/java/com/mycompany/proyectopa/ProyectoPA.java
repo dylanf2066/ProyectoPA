6ab7624237d44c680f0ad4d1c57a31b735aba5ae
day666 <bellitodylan@gmail.com>
2025-11-23 14:06:18 -0600

    try1

---
+++ src/main/java/com/mycompany/proyectopa/ProyectoPA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectopa;
+
+/**
+ *
+ * @author dylan
+ */
+public class ProyectoPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
